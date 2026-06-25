4ed90fb37cea146782b05abc0a748eb01e1c57f7
samuelafanou4-source <samuelafanou4@gmail.com>
2026-06-25 16:02:06 +0000

    creation du post

--- g_m_p_backend/main.py
+++ g_m_p_backend/main.py
@@ -17,3 +17,25 @@ class passwordResponse(BaseModel):
 
 fake_db: List[dict] = []
 id_counter = 1
+
+# --- LES OPÉRATIONS CRUD ---
+
+# 1. CREATE : Stocker un mot de passe existant
+
+@app.post("/passwords", response_model=passwordResponse, status_code=201)
+def create_password(item: passwordItem):
+    global id_counter
+
+    # Éviter les doublons pour un même site
+    for entry in fake_db:
+        if entry["site_name"].lower() == item.site_name.lower():
+            raise HTTPException(status_code=400, detail="Un mot de passe existe déjà pour ce site")
+        new_entry = {
+        "id": id_counter,
+        "site_name": item.site_name,
+        "username": item.username,
+        "password": item.password  # Reçu directement du client
+    }
+    fake_db.append(new_entry)
+    id_counter += 1
+    return new_entry
